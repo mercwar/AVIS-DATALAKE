1fc0fcb933be0051caf95cf56117251d61989981
CVBGOD <mercwar01@gmail.com>
2026-02-12 17:42:26 -0500

    Update fire-end.asm

--- fire-gem/fire-end.asm
+++ fire-gem/fire-end.asm
@@ -1,7 +1,6 @@
 section .text
-    global fire_end_entry
+    global fire_end_entry  ; This MUST be global for the linker to see it
 
 fire_end_entry:
-    ; [Logic: Parse fire-gem.bin header 'GEM!']
-    ; [Logic: Execute Command Stream 0x01-0x08]
+    ; Binary command logic (0x01 - 0x08)
     ret
