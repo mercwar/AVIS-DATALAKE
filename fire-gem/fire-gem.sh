64eb443c3392d299888e38e5b85e171100fc738f
CVBGOD <mercwar01@gmail.com>
2026-02-12 17:52:23 -0500

    Update fire-gem.sh

--- fire-gem/fire-gem.sh
+++ fire-gem/fire-gem.sh
@@ -2,11 +2,12 @@
 # AVIS-DATALAKE/fire-gem/fire-gem.sh
 GUID=$1
 
-# 1. Compile both ASM modules
+# 1. Compile both ASM modules to object files (.o)
 nasm -f elf64 fire-gem.asm -o fire-gem-asm.o
 nasm -f elf64 fire-end.asm -o fire-end-asm.o
 
-# 2. Link with C Engine (fixes 'undefined reference to fire_end_entry')
+# 2. Link ALL objects together with the C engine
+# You MUST include fire-end-asm.o here to resolve the reference
 gcc fire-gem.c fire-gem-asm.o fire-end-asm.o -o fire-gem -no-pie -z noexecstack
 
 # 3. RUN: Boot the Engine
