6902a7f971477b3ca0fdd91532a03b1311a1a73d
CVBGOD <mercwar01@gmail.com>
2026-02-12 18:20:14 -0500

    Update fire-gem.asm

--- fire-gem/fire-gem.asm
+++ fire-gem/fire-gem.asm
@@ -1,23 +1,27 @@
 ; AVIS-DATALAKE/fire-gem/fire-gem.asm
 section .text
-    global _start       ; THE NEW ENTRY POINT
-    extern fire_end_entry
+    global _start           ; Standard Entry for 'ld'
+    global run_asm_logic    ; Exported for the .so
+    extern fire_end_entry   ; Located in fire-end.asm
 
 _start:
-    ; 1. Ingest GUID from Stack (passed via BASH)
-    pop rdi             ; Get argc
-    pop rsi             ; Get program name
-    pop rdx             ; This is our 32-bit GUID argument
+    ; 1. Ingest GUID from the Stack (passed via BASH)
+    ; Stack on entry: [argc] [argv[0]] [argv[1]]
+    pop r8                  ; argc
+    pop r8                  ; argv[0] (program name)
+    pop rdi                 ; argv[1] (This is our 32-bit GUID)
 
-    ; 2. PROVISIONING: Copy KB Install Shield to Disk
-    ; 3. INJECTION: Append [UNINSTALL] to fire-gem.ini
+    call run_asm_logic
 
-    ; 4. MANUAL TRIGGER: Call the Binary Executor
-    call fire_end_entry
-
-    ; 5. EXIT: System Call to close the process
-    mov rax, 60         ; sys_exit
-    xor rdi, rdi        ; status 0
+    ; 5. EXIT
+    mov rax, 60             ; sys_exit
+    xor rdi, rdi
     syscall
+
+run_asm_logic:
+    ; RDI contains the GUID
+    ; [PROVISIONING]: Sync KB and inject [UNINSTALL] to fire-gem.ini
+    call fire_end_entry
+    ret
 
 section .note.GNU-stack noalloc noexec nowrite progbits
