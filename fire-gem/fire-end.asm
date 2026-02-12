c373bff3e12c036a891b512d11eac7d9c154cdc1
CVBGOD <mercwar01@gmail.com>
2026-02-12 18:03:09 -0500

    Update fire-end.asm

--- fire-gem/fire-end.asm
+++ fire-gem/fire-end.asm
@@ -1,11 +1,12 @@
 ; AVIS-DATALAKE/fire-gem/fire-end.asm
 section .text
-    global fire_end_entry
+    global fire_end_entry  ; Makes the symbol visible to the linker
 
 fire_end_entry:
-    ; 1. Ingest fire-gem.bin (Header: 0x47454D21)
-    ; 2. Execute Single-App Commands (0x01 - 0x08)
+    ; 1. Verify "GEM!" Binary Header
+    ; 2. Execute Single-App Commands (01-08)
+    ; 3. Finalize KB Installation
     ret
 
-; Security note to silence linker warning
+; Safety section to satisfy modern linkers
 section .note.GNU-stack noalloc noexec nowrite progbits
