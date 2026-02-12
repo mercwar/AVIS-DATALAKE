d7f8c8fa13d9a471af910124c961e98e63c7ba6e
CVBGOD <mercwar01@gmail.com>
2026-02-12 17:21:45 -0500

    Update fire-gem.asm

--- fire-gem/fire-gem.asm
+++ fire-gem/fire-gem.asm
@@ -1,14 +1,14 @@
 ; AVIS-DATALAKE/fire-gem/fire-gem.asm
 section .text
     global run_asm_logic
-    extern fire_end_entry ; Manual trigger to the new file
+    extern fire_end_entry ; The manual trigger link
 
 run_asm_logic:
-    ; RDI = GUID (passed from C)
-    ; 1. [PROVISION]: Read kb_install_shield.ini [FILES] section
-    ; 2. [SYNC]: Copy drivers/stubs to AVIS-DATALAKE/fire-gem/
-    ; 3. [INJECT]: Copy [UNINSTALL] from KB into living fire-gem.ini
-
-    ; 4. [MANUAL TRIGGER]: Start fire-end.asm to process BIN commands
+    ; RDI = guid_val (passed from C)
+    
+    ; 1. [SYNC]: Copy drivers/stubs from KB to living_dir
+    ; 2. [INJECT]: Copy [UNINSTALL] from KB Master to fire-gem.ini on disk
+    
+    ; 3. [MANUAL TRIGGER]: Jump to fire-end.asm before returning
     call fire_end_entry
     ret
