41e0215d9519ca6562be668c807b038051a2910d
CVBGOD <mercwar01@gmail.com>
2026-02-12 17:59:57 -0500

    Update fire-end.asm

--- fire-gem/fire-end.asm
+++ fire-gem/fire-end.asm
@@ -1,9 +1,11 @@
+; AVIS-DATALAKE/fire-gem/fire-end.asm
 section .text
     global fire_end_entry
 
 fire_end_entry:
-    ; 1. Verify "GEM!" Header
-    ; 2. Run Commands 01-08
+    ; 1. Ingest fire-gem.bin (Header: 0x47454D21)
+    ; 2. Execute Single-App Commands (0x01 - 0x08)
     ret
 
+; Security note to silence linker warning
 section .note.GNU-stack noalloc noexec nowrite progbits
