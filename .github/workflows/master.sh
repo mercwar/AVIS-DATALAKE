b7b7605838aa7639f0fb8b982f38538dff0fa42c
CVBGOD <mercwar01@gmail.com>
2026-02-12 09:01:09 -0500

    Delete .github directory

--- .github/workflows/master.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Master script: Initializing..."
-./worker.sh
-echo "Master script: Worker finished, resuming."
--- .github/workflows/worker.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Worker script: Performing tasks..."
-sleep 2
-echo "Worker script: Task complete."
