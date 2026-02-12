81d43bec27d701dca93f7ddb50c31fb04daa6589
CVBGOD <mercwar01@gmail.com>
2026-02-12 17:28:00 -0500

    Update fire-end.asm

--- fire-gem/fire-end.asm
+++ fire-gem/fire-end.asm
@@ -1,9 +1,7 @@
-; AVIS-DATALAKE/fire-gem/fire-end.asm
 section .text
     global fire_end_entry
 
 fire_end_entry:
-    ; 1. LOAD: Open fire-gem.bin
-    ; 2. PARSE: Verify Header 0x47454D21 ("GEM!")
-    ; 3. EXEC: Run Commands 0x01 through 0x08
+    ; [Logic: Parse fire-gem.bin header 'GEM!']
+    ; [Logic: Execute Command Stream 0x01-0x08]
     ret
