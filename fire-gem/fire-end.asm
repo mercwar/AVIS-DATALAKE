0af01bcc90ada49da2cbc0b01356c4f205a180aa
CVBGOD <mercwar01@gmail.com>
2026-02-12 17:56:41 -0500

    Update fire-end.asm

--- fire-gem/fire-end.asm
+++ fire-gem/fire-end.asm
@@ -1,11 +1,9 @@
-; AVIS-DATALAKE/fire-gem/fire-end.asm
 section .text
     global fire_end_entry
 
 fire_end_entry:
-    ; 1. Process BIN Header (GEM!)
-    ; 2. Execute Command Stream (0x01 - 0x08)
-    ; 3. Finalize: LittleBot writes to fire-gem.log
+    ; 1. Verify "GEM!" Header
+    ; 2. Run Commands 01-08
     ret
 
 section .note.GNU-stack noalloc noexec nowrite progbits
