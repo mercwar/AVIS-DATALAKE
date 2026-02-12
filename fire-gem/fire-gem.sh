a6e5986ce28421ab4dd46aef4df203b053680852
CVBGOD <mercwar01@gmail.com>
2026-02-12 17:49:55 -0500

    Update fire-gem.sh

--- fire-gem/fire-gem.sh
+++ fire-gem/fire-gem.sh
@@ -2,13 +2,12 @@
 # AVIS-DATALAKE/fire-gem/fire-gem.sh
 GUID=$1
 
-# 1. PREP: Compile BOTH ASM modules to disk
+# 1. Compile both ASM modules
 nasm -f elf64 fire-gem.asm -o fire-gem-asm.o
 nasm -f elf64 fire-end.asm -o fire-end-asm.o
 
-# 2. LINK: Include both .o files and the C file to create the binary
-# This fixes 'undefined reference to fire_end_entry'
+# 2. Link with C Engine (fixes 'undefined reference to fire_end_entry')
 gcc fire-gem.c fire-gem-asm.o fire-end-asm.o -o fire-gem -no-pie -z noexecstack
 
-# 3. RUN: Open C Engine with the GUID loaded from the INI
+# 3. RUN: Boot the Engine
 ./fire-gem "$GUID"
