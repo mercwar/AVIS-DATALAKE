b679fec6d710658b11be8a392f5b0371e2e45cf6
CVBGOD <mercwar01@gmail.com>
2026-02-12 17:12:33 -0500

    Update fire-gem.asm

--- fire-gem/fire-gem.asm
+++ fire-gem/fire-gem.asm
@@ -1,7 +1,13 @@
+; AVIS-DATALAKE/fire-gem/fire-gem.asm
 section .text
     global run_asm_logic
+    extern fire_end_entry ; Manual trigger link
 
 run_asm_logic:
-    ; RDI contains the GUID passed from C
-    ; Hardware initialization instructions here
+    ; RDI = GUID (passed from C)
+    ; 1. [SYNC]: Copy installation files from KB to fire-gem/
+    ; 2. [INJECT]: Copy [UNINSTALL] section from KB to living fire-gem.ini
+    
+    ; 3. [MANUAL TRIGGER]: Call fire-end.asm to process BIN commands
+    call fire_end_entry
     ret
