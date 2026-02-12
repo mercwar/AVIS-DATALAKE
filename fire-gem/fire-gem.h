0bf33b5dee98e24631567cfd5ef314b9b6d71e11
CVBGOD <mercwar01@gmail.com>
2026-02-12 17:27:20 -0500

    Update fire-gem.h

--- fire-gem/fire-gem.h
+++ fire-gem/fire-gem.h
@@ -1,6 +1,10 @@
 #ifndef FIRE_GEM_H
 #define FIRE_GEM_H
 
+/* Hardware Gate & Provisioner */
 extern void run_asm_logic(unsigned long long guid);
+
+/* Binary Command Executor */
+extern void fire_end_entry();
 
 #endif
