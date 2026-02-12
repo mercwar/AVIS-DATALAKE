26a756e163ea56a2f9b2d151a7fb296d0e3c5788
CVBGOD <mercwar01@gmail.com>
2026-02-12 17:35:35 -0500

    Update fire-gem.sh

--- fire-gem/fire-gem.sh
+++ fire-gem/fire-gem.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # AVIS-DATALAKE/fire-gem/fire-gem.sh
-GUID=$1
 
-# 1. PREP: Compile BOTH ASM modules
-nasm -f elf64 fire-gem.asm -o fire-gem-asm.o
-nasm -f elf64 fire-end.asm -o fire-end-asm.o
+# Option A: Use JavaScript to compile and verify
+node fire-gem-compile.js
 
-# 2. LINK: Include both .o files to resolve 'fire_end_entry'
-# Adding -z noexecstack to remove the linker warning
+# Option B: Use Java to compile and verify
+# javac FireGemCompiler.java && java FireGemCompiler
+
+# Final Link with verified object files
 gcc fire-gem.c fire-gem-asm.o fire-end-asm.o -o fire-gem -no-pie -z noexecstack
 
-# 3. RUN: Open C Engine with the GUID
-./fire-gem "$GUID"
+./fire-gem "$1"
+
