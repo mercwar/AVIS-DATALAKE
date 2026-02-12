0d63a59b915c7d1afe63842188d3f68072f3e406
CVBGOD <mercwar01@gmail.com>
2026-02-12 16:23:35 -0500

    Update fire-gem.h

--- fire-gem/fire-gem.h
+++ fire-gem/fire-gem.h
@@ -1,9 +1,7 @@
 #ifndef FIRE_GEM_H
 #define FIRE_GEM_H
 
-#define MANIFEST "fire-gem.ini"
-
-// Extern link to the ASM logic
+/* Extern declaration matches the ASM entry point */
 extern void run_asm_logic(unsigned long long guid);
 
 #endif
