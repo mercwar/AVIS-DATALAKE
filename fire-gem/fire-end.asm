c3c14fd87cedeaf7f294a55382d8d68b06c2f077
CVBGOD <mercwar01@gmail.com>
2026-02-12 17:22:22 -0500

    Update fire-end.asm

--- fire-gem/fire-end.asm
+++ fire-gem/fire-end.asm
@@ -3,9 +3,7 @@ section .text
     global fire_end_entry
 
 fire_end_entry:
-    ; 1. OPEN fire-gem.bin
-    ; 2. VERIFY HEADER: 0x47454D21 ("GEM!")
-    ; 3. EXECUTE COMMANDS: 0x01 (INIT) -> 0x08 (FINALIZE)
-    
-    ; Logic to finalize single app installation
+    ; 1. LOAD: Open fire-gem.bin
+    ; 2. PARSE: Verify Header 0x47454D21 ("GEM!")
+    ; 3. EXEC: Run Commands 0x01 through 0x08
     ret
