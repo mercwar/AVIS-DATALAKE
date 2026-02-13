bb3c801c7af731acb681c8ebe460e0e24feef21c
CVBGOD <mercwar01@gmail.com>
2026-02-12 19:41:27 -0500

    Update manifest.asm

--- engine/manifest.asm
+++ engine/manifest.asm
@@ -1,54 +1,80 @@
-; ==========================================================
-; ENGINE: manifest.asm (CVBGOD V4 MASTER - FIXED)
-; PURPOSE: 4-Byte Header Ingestion -> Direct Manifestation
-; ==========================================================
 section .data
-    shell db "/bin/bash", 0
-    arg_c db "-c", 0
-    ; The Logic: Ingests kb.bin ($1) -> Extends the chain
-    logic db "SZ=$(od -An -N4 -tu4 '$1'); "
-          db "HDR=$(dd if='$1' bs=1 skip=4 count=$SZ); "
-          db "echo $HDR | jq -c '.[]' | while read i; do "
-          db "N=$(echo $i | jq -r '.nest'); F=$(echo $i | jq -r '.file'); "
-          db "C=$(echo $i | jq -r '.nest_cmd'); mkdir -p $N; "
-          db "dd if='$1' bs=1 skip=$((4 + SZ)) > $N/$F; "
-          db "[ \"$C\" != \"null\" ] && eval $(echo $C | sed \"s|%n|$N|g; s|%f|$F|g\"); "
-          db "done", 0
+    kb_src   db "KB.bin", 0
+    fs_dest  db "fire-shield", 0
+    log_file db "fire-gem.log", 0
+    msg_inst db "[LITTLEBOT] KB.bin detected. Installing Fire-Shield...", 0xA
+    msg_len  equ $ - msg_inst
+    buffer   times 1024 db 0
 
 section .text
     global _start
 
 _start:
-    ; 1. Hardware Sync (16-byte Stack Alignment)
-    mov rbp, rsp
-    and rsp, -16
+    ; 1. LOG START: Write to fire-gem.log
+    mov rax, 2          ; sys_open
+    mov rdi, log_file
+    mov rsi, 1089       ; O_WRONLY | O_CREAT | O_APPEND
+    mov rdx, 0664q
+    syscall
+    push rax            ; Save log FD
 
-    ; 2. Acquire Fuel Path (kb.bin from argv)
-    pop rax             ; argc
-    pop rax             ; prog_name
-    pop r8              ; r8 = path to kb.bin
-    test r8, r8
-    jz exit
-
-    ; 3. Bridge Pointers (LEA Fix for syntax error)
-    lea rdi, [rel shell]
-    lea rsi, [rel arg_c]
-    lea rdx, [rel logic]
-
-    ; 4. Execute (sys_execve)
-    mov rax, 59         
-    
-    push 0              ; NULL env
-    push r8             ; $1 = kb.bin
-    push rdx            ; $0 = logic
-    push rsi            ; -c
-    push rdi            ; /bin/bash
-    
-    mov rsi, rsp        ; argv
-    xor rdx, rdx        ; envp
+    mov rdi, rax
+    mov rax, 1          ; sys_write
+    mov rsi, msg_inst
+    mov rdx, msg_len
     syscall
 
-exit:
-    mov rax, 60
-    xor rdi, rdi
+    ; 2. OPEN KB.bin (The Source)
+    mov rax, 2          ; sys_open
+    mov rdi, kb_src
+    mov rsi, 0          ; O_RDONLY
+    syscall
+    mov r8, rax         ; Save Source FD
+
+    ; 3. CREATE fire-shield (The Destination App)
+    mov rax, 85         ; sys_creat
+    mov rdi, fs_dest
+    mov rsi, 0755o      ; rwxr-xr-x
+    syscall
+    mov r9, rax         ; Save Dest FD
+
+    ; 4. OFFSET: Skip 4-byte header "GEM!"
+    mov rax, 8          ; sys_lseek
+    mov rdi, r8
+    mov rsi, 4
+    mov rdx, 0          ; SEEK_SET
+    syscall
+
+    ; 5. COPY LOOP (KB -> DISK)
+copy_loop:
+    mov rax, 0          ; sys_read
+    mov rdi, r8
+    mov rsi, buffer
+    mov rdx, 1024
+    syscall
+    test rax, rax       ; Check EOF
+    jz finalize
+
+    mov rdx, rax        ; Write bytes read
+    mov rax, 1          ; sys_write
+    mov rdi, r9
+    mov rsi, buffer
+    syscall
+    jmp copy_loop
+
+finalize:
+    ; 6. CLOSE ALL
+    mov rax, 3          ; sys_close
+    mov rdi, r8
+    syscall
+    mov rdi, r9
+    syscall
+    pop rdi             ; Get log FD
+    syscall
+
+    ; 7. EXECUTE FIRE-SHIELD
+    mov rax, 59         ; sys_execve
+    mov rdi, fs_dest
+    xor rsi, rsi
+    xor rdx, rdx
     syscall
