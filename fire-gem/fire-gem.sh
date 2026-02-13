573f6ee21d30fcb1c91afcf22038ce35efa550ff
CVBGOD <mercwar01@gmail.com>
2026-02-12 19:30:00 -0500

    Update fire-gem.sh

--- fire-gem/fire-gem.sh
+++ fire-gem/fire-gem.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
-# AVIS-DATALAKE/fire-gem/fire-gem.sh
-
-GUID=$1
-
-# 1. COMPILE: Build the single ASM file into an object
+# 1. Assemble to object file
 nasm -f elf64 fire-gem.asm -o fire-gem.o
 
-# 2. LINK: Create the standalone binary (No C files, No .so)
-# Using ld avoids the 'multiple definition of _start' error
+# 2. Link to executable
 ld fire-gem.o -o fire-gem
 
-# 3. RUN: Execute the bootloader with the 32-bit GUID
-if [ -f "./fire-gem" ]; then
-    chmod +x fire-gem
-    ./fire-gem "$GUID"
-else
-    echo "[CRITICAL] Link failed."
-    exit 1
-fi
+# 3. RUN
+./fire-gem
